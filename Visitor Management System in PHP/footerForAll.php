5e6e2d70e99afa6d128765118d0339901d7cde47
milesgiges <milesgiges@gmail.com>
2022-10-27 12:19:12 +0300

    initial commit

---
+++ Visitor Management System in PHP/footerForAll.php
@@ -0,0 +1,17 @@
+<?php {?>
+
+<link rel="stylesheet" type="text/css" href="footer.css"> 
+
+
+	<div class = "footer">
+		
+	<span > Ray Abajila....2022
+	</span>
+
+
+	</div>
+
+
+
+
+<?php }?>
---
+++ Visitor Management System in PHP/form.php
@@ -0,0 +1,34 @@
+<html>
+
+body{
+background-image: linear-gradient(greenyellow,#46b8da);
+}
+<form action="" method = "POST">
+
+<label>Name : </label><input name = "name" type = "text">
+<label>C No. :</label><input name = "cno" type = "number">
+<input name = "submit" type = "submit">
+</form>
+
+
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+echo $_POST["name"];
+if(strlen($_POST["cno"]) !=10)
+echo "wrong input";
+else
+echo $_POST["cno"];
+
+
+}
+
+
+
+
+
+?>
+
+</html>
